8de76b5f17f73bfd475d106eed98a05d2710a6aa
Kim Ipsen <kim.ipsen@outlook.dk>
2023-12-01 12:06:44 +0100

    keys

--- keyboards/sofle/rev1/keymaps/kimipsen/config.h
+++ keyboards/sofle/rev1/keymaps/kimipsen/config.h
@@ -7,6 +7,9 @@ for more options.
 #define SOFT_SERIAL_PIN D2
 #define ENCODER_DIRECTION_FLIP
 
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+
 #define LAYER_STATE_8BIT
 
 
