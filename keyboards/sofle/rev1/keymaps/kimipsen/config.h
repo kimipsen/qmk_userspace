1d2eed93c41eae5419366aa09813d6637df5d07e
Kim Ipsen <kim.ipsen@outlook.dk>
2023-12-01 12:14:13 +0100

    simple

--- keyboards/sofle/rev1/keymaps/kimipsen/config.h
+++ keyboards/sofle/rev1/keymaps/kimipsen/config.h
@@ -4,20 +4,20 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
-#define SOFT_SERIAL_PIN D2
-#define ENCODER_DIRECTION_FLIP
+// #define SOFT_SERIAL_PIN D2
+// #define ENCODER_DIRECTION_FLIP
 
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+// #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+// #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
-#define LAYER_STATE_8BIT
+// #define LAYER_STATE_8BIT
 
 
-// RGB
-#define RGBLIGHT_SPLIT
-#define RGBLIGHT_SLEEP
-#define RGBLIGHT_DEFAULT_ON true
-#define RGBLIGHT_LAYERS
-#define RGBLED_NUM 72
-#define WS2812_DI_PIN D3
-#define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
+// // RGB
+// #define RGBLIGHT_SPLIT
+// #define RGBLIGHT_SLEEP
+// #define RGBLIGHT_DEFAULT_ON true
+// #define RGBLIGHT_LAYERS
+// #define RGBLED_NUM 72
+// #define WS2812_DI_PIN D3
+// #define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
