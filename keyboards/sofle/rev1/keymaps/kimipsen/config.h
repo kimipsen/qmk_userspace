89ee556253ce2f9ef5d7a5d224e1a97935877d2a
Kim Ipsen <kim.ipsen@outlook.dk>
2023-12-01 12:21:43 +0100

    rgb

--- keyboards/sofle/rev1/keymaps/kimipsen/config.h
+++ keyboards/sofle/rev1/keymaps/kimipsen/config.h
@@ -10,14 +10,14 @@ for more options.
 // #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
 // #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
-// #define LAYER_STATE_8BIT
+#define LAYER_STATE_8BIT
 
 
 // // RGB
 // #define RGBLIGHT_SPLIT
 // #define RGBLIGHT_SLEEP
 // #define RGBLIGHT_DEFAULT_ON true
-// #define RGBLIGHT_LAYERS
-// #define RGBLED_NUM 72
+#define RGBLIGHT_LAYERS
+#define RGBLED_NUM 72
 // #define WS2812_DI_PIN D3
-// #define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
+#define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
