9200f7f6b113023ff69cc01a9c0766b7b140a8d2
Kim Ipsen <kim.ipsen@outlook.dk>
2023-12-01 13:11:48 +0100

    rgb test

--- keyboards/sofle/rev1/keymaps/kimipsen/config.h
+++ keyboards/sofle/rev1/keymaps/kimipsen/config.h
@@ -15,14 +15,38 @@ for more options.
 
 // // RGB
 // #define RGBLIGHT_SPLIT
-// #define RGBLIGHT_SLEEP
 // #define RGBLIGHT_DEFAULT_ON true
-// #define RGBLIGHT_LAYERS
-// #define RGBLED_NUM 72
 // #define WS2812_DI_PIN D3
 // #define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
 
 #ifdef TAPPING_TERM
     #undef TAPPING_TERM
     #define TAPPING_TERM 200
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_SLEEP
+    #define RGBLIGHT_LAYERS
+    #undef RGBLED_NUM
+    #define RGBLED_NUM 72
+    #define RGBLED_SPLIT \
+        { 36, 36 } // haven't figured out how to use this yet
+
+    //#define RGBLIGHT_ANIMATIONS
+	//#define RGBLIGHT_EFFECT_BREATHING
+	#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+	//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+	//#define RGBLIGHT_EFFECT_SNAKE
+	//#define RGBLIGHT_EFFECT_KNIGHT
+    //#define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    //#define RGBLIGHT_EFFECT_RGB_TEST
+	//#define RGBLIGHT_EFFECT_ALTERNATING
+	//#define RGBLIGHT_EFFECT_TWINKLE
+    //#define RGBLED_SPLIT
+
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
 #endif
